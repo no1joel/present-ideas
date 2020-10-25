97a27280a7d951415d1d947ee0529f95498989f7
Joel <no1joel@btinternet.com>
2020-10-25 17:35:32 +0000

    Add Layout component

--- present_ideas/present_ideas/static/components/LandingPage.vue
+++ present_ideas/present_ideas/static/components/LandingPage.vue
@@ -1,35 +1,39 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col">
-        <div class="jumbotron">
-          <p>Please pick <strong>your name</strong> to get started.</p>
-          <div class="form-row">
-            <PeoplePicker
-              class="col-sm-10"
-              v-bind:loading="loading"
-              v-bind:people="people"
-              v-model="currentUser"
-            />
-            <button
-              type="button"
-              class="btn btn-block btn-success col-sm-2"
-              v-bind:disabled="!currentUser"
-            >
-              Go
-            </button>
+  <Layout>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <div class="jumbotron">
+            <p>Please pick <strong>your name</strong> to get started.</p>
+            <div class="form-row">
+              <PeoplePicker
+                class="col-sm-10"
+                v-bind:loading="loading"
+                v-bind:people="people"
+                v-model="currentUser"
+              />
+              <button
+                type="button"
+                class="btn btn-block btn-success col-sm-2"
+                v-bind:disabled="!currentUser"
+              >
+                Go
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </Layout>
 </template>
 
 <script>
+import Layout from "../layout/Layout.vue";
 import PeoplePicker from "./PeoplePicker.vue";
 
 export default {
   components: {
+    Layout,
     PeoplePicker,
   },
   data() {
---
+++ present_ideas/present_ideas/static/layout/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">
+          Present Ideas
+        </li>
+      </ol>
+    </nav>
+    <slot />
+  </div>
+</template>
