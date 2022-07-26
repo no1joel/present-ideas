da3d7fcdf5f8ce3a123ab1e051a7dba9d1d821a0
Joel <no1joel@btinternet.com>
2022-07-26 18:45:54 +0100

    Add tests for TheirListPage

---
+++ present_ideas/present_ideas/static/pages/__tests__/TheirListPage.test.js
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/vue";
+import { createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
+import getStore from "../../store";
+
+import TheirListPage from "../TheirListPage.vue";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe("TheirListPage", () => {
+  // TODO: Just trying to get it to pass right now.
+  // shouldn't mock fetch, shouldn't mess with global
+  // Should probably use what vfp used with the api.
+  beforeEach(() => {
+    const json = async () => ({
+      presents: [],
+    });
+    const response = { json };
+    global.fetch = async () => response;
+  });
+  it("matches snapshot", () => {
+    const mockRoute = { params: {} };
+    const store = getStore();
+    const mocks = { $route: mockRoute };
+    const renderOptions = { mocks, localVue, store };
+
+    expect(render(TheirListPage, renderOptions).container).toMatchSnapshot();
+  });
+});
