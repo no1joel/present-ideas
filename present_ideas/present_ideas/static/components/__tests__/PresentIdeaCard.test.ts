9f786052caf94430831959c1fd315ef31c63ae1d
Joel <no1joel@btinternet.com>
2022-08-14 10:09:31 +0100

    Add PresentIdeaCard snapshot test

---
+++ present_ideas/present_ideas/static/components/__tests__/PresentIdeaCard.test.ts
@@ -0,0 +1,18 @@
+import { render } from "../../pages/__tests__/utils";
+import PresentIdeaCard from "../PresentIdeaCard.vue";
+
+describe("PresentIdeaCard", () => {
+  it("matches snapshot", () => {
+    expect(render(PresentIdeaCard).container).toMatchSnapshot();
+  });
+  it.todo("shows loading");
+  it.todo("shows the right title");
+  it.todo("shows the right notes");
+  it.todo("shows the right price");
+  it.todo("shows unclaimed if unclaimed");
+  it.todo("shows who claimed it");
+  it.todo("doesn't show claimed it if it's for you");
+  it.todo("shows who added it");
+  it.todo("lets you delete it if you added it");
+  it.todo("lets you claim it if it's not yours");
+});
