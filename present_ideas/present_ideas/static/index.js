5a0010f58ed2a7823dfec47240eb362cd6d413bd
Joel <no1joel@btinternet.com>
2022-10-17 14:42:50 +0100

    Remove console.log

--- present_ideas/present_ideas/static/index.js
+++ present_ideas/present_ideas/static/index.js
@@ -7,16 +7,13 @@ import getStore from "./store";
 const store = getStore();
 
 router.beforeEach((to, from, next) => {
-  console.log("hello from the router!", to.path, to.name, from.path, from.name);
   const currentUser = store.getters.currentUser;
   const viewingUser = store.getters.viewingUser;
   if (to.name === "list") {
     if (to.params.user !== currentUser) {
-      console.log("updatingCurrentUser from route", to.params.user);
       store.dispatch("setCurrentUser", { currentUser: to.params.user });
     }
     if (to.params.viewing !== viewingUser) {
-      console.log("updatingViewingUser from route", to.params.viewing);
       store.dispatch("setViewingUser", { viewingUser: to.params.viewing });
     }
   }
